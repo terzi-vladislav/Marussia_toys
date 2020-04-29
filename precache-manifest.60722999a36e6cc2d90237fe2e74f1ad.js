9b4fec866e6497db3b0ca2e4e67d0846684b9e33
terzi-vladislav <terzi.va@phystech.edu>
2020-04-29 15:29:13 +0300

    Updates

---
+++ precache-manifest.60722999a36e6cc2d90237fe2e74f1ad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf10c0de4dcbdbb1591c42f6b162814",
+    "url": "/Marussia_toys/index.html"
+  },
+  {
+    "revision": "1d9e54f05e78d12bc6b3",
+    "url": "/Marussia_toys/static/css/main.de49c029.chunk.css"
+  },
+  {
+    "revision": "1ebc14752859d9887ec1",
+    "url": "/Marussia_toys/static/js/2.c3d2d99f.chunk.js"
+  },
+  {
+    "revision": "54d43f5ccaea50614fd60281adc195ad",
+    "url": "/Marussia_toys/static/js/2.c3d2d99f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d9e54f05e78d12bc6b3",
+    "url": "/Marussia_toys/static/js/main.92aae0b1.chunk.js"
+  },
+  {
+    "revision": "760b5cde1239a822c352",
+    "url": "/Marussia_toys/static/js/runtime-main.2ffd5e93.js"
+  },
+  {
+    "revision": "e930720b3d5a79313db4f3e253589872",
+    "url": "/Marussia_toys/static/media/backgroundHome.e930720b.JPG"
+  },
+  {
+    "revision": "ed9d0281264c91a26fde915ed8e2f8e5",
+    "url": "/Marussia_toys/static/media/backgroundShop.ed9d0281.heic"
+  },
+  {
+    "revision": "112fa5a775ccce7a4c1d009b01766349",
+    "url": "/Marussia_toys/static/media/cacti.112fa5a7.heic"
+  },
+  {
+    "revision": "6def455bd7c2b9de4b88ff6774e651a5",
+    "url": "/Marussia_toys/static/media/special.6def455b.jpg"
+  },
+  {
+    "revision": "f139be724ccbc1dfbde04996fb2273fa",
+    "url": "/Marussia_toys/static/media/toys.f139be72.jpg"
+  }
+]);
